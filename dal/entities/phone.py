d303de5378542eaa1cbed39ba9b2336d9eb26ead
Roman Kulchytskiy <buma.ua@gmail.com>
2025-11-15 18:28:32 +0200

    Fix phone tests

--- dal/entities/phone.py
+++ dal/entities/phone.py
@@ -1,5 +1,3 @@
-import re
-
 from dal.entities.field import Field
 
 
@@ -7,5 +5,8 @@ class Phone(Field):
     def __init__(self, value: str):
         if not isinstance(value, str):
             raise TypeError(f"Phone value should be str not {type(value).__name__}")
+
+        if not value or not value.strip():
+            raise ValueError("Phone value cannot be empty")
 
         super().__init__(value)
